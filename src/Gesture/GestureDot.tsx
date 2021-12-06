c2c3e9ea106abc1a5886c2ad3741ee9d3159b49f
chan vin sheng <vschan@snsoft.my>
2021-12-06 16:51:36 +0800

    Practice 1 : change background color when on specfic location

--- src/Gesture/GestureDot.tsx
+++ src/Gesture/GestureDot.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
-import { View, Text, Button, Dimensions } from 'react-native'
+import { View, Text, Button, Dimensions, ImageBackground } from 'react-native'
 import { PanGestureHandler, PanGestureHandlerGestureEvent } from 'react-native-gesture-handler'
-import Animated, { useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withDecay, withTiming } from 'react-native-reanimated'
+import Animated, { interpolateColor, useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withDecay, withTiming } from 'react-native-reanimated'
 import Svg, { Circle } from 'react-native-svg'
 
 
@@ -17,16 +17,20 @@ const GestureDot = () => {
 
     const translateX = useSharedValue(0);
     const translateY = useSharedValue(0);
-
-
+    const backgroundValue = useSharedValue("white");
 
     const vStyle = useAnimatedStyle(() => {
         return {
             transform: [
                 { translateX: translateX.value },
                 { translateY: translateY.value }
-            ]
+            ],
+        }
+    })
 
+    const bStyle = useAnimatedStyle(() => {
+        return {
+            backgroundColor: backgroundValue.value
         }
     })
 
@@ -37,11 +41,10 @@ const GestureDot = () => {
             ctx.translateY = translateY.value;
         },
         onActive: (event, ctx) => {
-            console.log(event)
-            console.log(windowHeight)
-            console.log(event.translationY + ctx.translateY)
+
             const x = event.translationX + ctx.translateX
             const y = event.translationY + ctx.translateY
+
             if (y < 0) translateY.value = 0;
             else if (y > windowHeight - 100) translateY.value = windowHeight - 100;
             else translateY.value = y;
@@ -49,6 +52,19 @@ const GestureDot = () => {
             if (x < 0) translateX.value = 0;
             else if (x > windowWidth - 100) translateX.value = windowWidth - 100;
             else translateX.value = x;
+
+            console.log("x : " + x)
+            console.log("y : " + y)
+            console.log(windowWidth - 100)
+            //top left
+            if (x < 100 && y < 100) backgroundValue.value = "red";
+            //top right
+            else if (x > windowWidth - 200 && y < 100) backgroundValue.value = "orange";
+            //bottom left
+            else if (x < 100 && y > windowHeight - 200) backgroundValue.value = "green";
+            //bottom right
+            else if (x > windowWidth - 200 && y > windowHeight - 200) backgroundValue.value = "aquamarine";
+            else backgroundValue.value = "white"
 
         },
         onEnd: (event, ctx) => {
@@ -60,13 +76,23 @@ const GestureDot = () => {
                 velocity: event.velocityY,
                 clamp: [0, windowHeight - 100],
             });
+
+
+
         }
     })
 
     return (
-        <PanGestureHandler onGestureEvent={panGesture}  >
-            <AnimatedView style={[vStyle, { height: 100, width: 100, backgroundColor: "blue" }]} />
-        </PanGestureHandler>
+        <AnimatedView style={[bStyle, { height: windowHeight }]} >
+            <PanGestureHandler onGestureEvent={panGesture}  >
+                <AnimatedView style={[vStyle, { height: 100, width: 100, backgroundColor: "blue" }]} />
+            </PanGestureHandler>
+            <Svg width={windowWidth} height={windowHeight} style={{ borderColor: "yellow", borderWidth: 10, position: "absolute" }} >
+                {/* <Circle r="180" stroke="black" />
+                <Circle r="180" stroke="black" cx={windowWidth} /> */}
+            </Svg>
+        </AnimatedView>
+
     )
 }
 
